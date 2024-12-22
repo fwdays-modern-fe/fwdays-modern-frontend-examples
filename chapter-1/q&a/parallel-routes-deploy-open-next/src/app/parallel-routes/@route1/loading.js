ba1e32b9c28ad2e25d49a591324f31e043a73779
akhudiakov97 <akhudiakov97@gmail.com>
2024-12-22 11:19:42 +0000

    Q&A examples

---
+++ chapter-1/q&a/parallel-routes-deploy-open-next/src/app/parallel-routes/@route1/loading.js
@@ -0,0 +1,13 @@
+export default function Loading() {
+  return (
+      <div className="loading-container">
+        <div className="loading-spinner">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <p className="loading-text">Завантажуємо Route 1</p>
+      </div>
+  );
+}
