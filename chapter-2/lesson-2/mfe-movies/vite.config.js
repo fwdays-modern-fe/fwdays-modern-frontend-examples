c8985d72d50df3ec03efec31333d5abd82c2a991
akhudiakov97 <akhudiakov97@gmail.com>
2025-01-04 23:49:41 +0000

    Fix

--- chapter-2/lesson-2/mfe-movies/vite.config.js
+++ chapter-2/lesson-2/mfe-movies/vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+import federation from "@originjs/vite-plugin-federation";
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+        react(),
+        federation({
+            name: "movies",
+            filename: "moviesRemoteEntry.js",
+            exposes: {
+                "./Movies": "./src/App.jsx",
+            },
+        }),
+    ],
+    build: {
+        modulePreload: false,
+        target: "esnext",
+        minify: false,
+        cssCodeSplit: false,
+    },
+});
--- chapter-2/lesson-2/mfe-wrapper/vite.config.ts
+++ chapter-2/lesson-2/mfe-wrapper/vite.config.ts
@@ -1,11 +1,25 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import TanStackRouterVite from "@tanstack/router-plugin/vite";
 
+import federation from "@originjs/vite-plugin-federation";
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-      react(),
-      TanStackRouterVite()
-  ],
+    plugins: [
+        react(),
+        TanStackRouterVite(),
+        federation({
+            name: "wrapper-app",
+            remotes: {
+                movies: "http://localhost:5001/assets/moviesRemoteEntry.js",
+            },
+        }),
+    ],
+    build: {
+        modulePreload: false,
+        target: "esnext",
+        minify: false,
+        cssCodeSplit: false,
+    },
 })
