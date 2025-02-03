8e94c4a03d965bb1fb5a32ca3230a9187ffe1496
twoninth <thanqdj@gmail.com>
2025-02-03 13:09:16 +0200

    Lesson 11: Introduction to reactive programming (RxJS, Observables)

---
+++ chapter-4/lesson-2/observables-into/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import ObservableExampleComponent from "@/ObservableExample.tsx";
+
+function App() {
+    return (
+        <>
+            <ObservableExampleComponent />
+        </>
+    );
+}
+
+export default App;
